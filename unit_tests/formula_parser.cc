77554d6b574c1dbf92c9ac2136baddf1c21ec229
Ondra Lengal <ondra.lengal@gmail.com>
2011-07-16 16:15:31 +0200

    unit_tests/formula_parser.hh: add parser of formulae for tests

---
+++ unit_tests/formula_parser.cc
@@ -0,0 +1,148 @@
+/*****************************************************************************
+ *  Vojnar's Army Tree Automata Library
+ *
+ *  Copyright (c) 2011  Ondra Lengal <ilengal@fit.vutbr.cz>
+ *
+ *  Description:
+ *    Implementation of FormulaParser class.
+ *
+ *****************************************************************************/
+
+#include "formula_parser.hh"
+
+// VATA headers
+#include <vata/convert.hh>
+#include <vata/vata.hh>
+
+using VATA::Private::Convert;
+
+FormulaParser::ParserResultBooleanType
+	FormulaParser::ParseExpressionBoolean(std::string input)
+{
+	boost::trim(input);
+	if (input.empty())
+	{	// in case the input is empty
+		return VariableListType();
+	}
+
+	std::vector<std::string> splitInput;
+	boost::algorithm::split(splitInput, input, boost::is_any_of("*"));
+
+	if (splitInput.empty())
+	{	// in case the splitting failed
+		throw std::runtime_error("boost::algorithm::split() returned "
+			"an invalid result");
+	}
+
+	VariableListType varList;
+	for (std::vector<std::string>::const_iterator it = splitInput.begin();
+		it != splitInput.end(); ++it)
+	{
+		std::string str = boost::trim_copy(*it);
+
+		if (str.length() == 0)
+		{
+			throw std::invalid_argument(__func__ + std::string(": invalid argument"));
+		}
+
+		VariableOccurenceType var("", true);
+
+		if (str[0] == '~')
+		{	// in case the variable is complemented
+			var.second = false;
+			str = str.substr(1);
+		}
+
+		var.first = str;
+
+		varList.push_back(var);
+	}
+
+	return varList;
+}
+
+
+FormulaParser::ParserResultUnsignedType
+	FormulaParser::ParseExpressionUnsigned(std::string input)
+{
+	boost::trim(input);
+	std::vector<std::string> splitInput;
+	boost::algorithm::split(splitInput, input, boost::is_any_of("="));
+
+	if (splitInput.size() != 2)
+	{	// in case the input looks strange
+		throw std::invalid_argument(__func__ + std::string(": invalid argument"));
+	}
+
+	return ParserResultUnsignedType(Convert::FromString<unsigned>(splitInput[1]),
+		ParseExpressionBoolean(splitInput[0]));
+}
+
+
+FormulaParser::ParserResultUnsignedVecType
+	FormulaParser::ParseExpressionUnsignedVec(std::string input)
+{
+	boost::trim(input);
+	std::vector<std::string> splitInput;
+	boost::algorithm::split(splitInput, input, boost::is_any_of("="));
+
+	if (splitInput.size() != 2)
+	{	// in case the input looks strange
+		throw std::invalid_argument(__func__ + std::string(": invalid argument "
+			"(not in the \"<formula> = <values>\" format)"));
+	}
+
+	std::string& secondPart = splitInput[1];
+	boost::trim(secondPart);
+
+	if ((secondPart[0] != '{') || (secondPart[secondPart.size() - 1] != '}'))
+	{	// in case the input is malformated
+		throw std::invalid_argument(__func__ + std::string(": invalid argument "
+			"(values are not in the \"{ <list> }\" format)"));
+	}
+
+	secondPart = secondPart.substr(1, secondPart.length() - 2);
+	boost::trim(secondPart);
+
+	if (secondPart.empty())
+	{	// in case the set is empty
+		return ParserResultUnsignedVecType(std::vector<unsigned>(),
+			ParseExpressionBoolean(splitInput[0]));
+	}
+
+	std::vector<std::string> splitSecondPart;
+	boost::algorithm::split(splitSecondPart, secondPart, boost::is_any_of(","));
+
+	if (splitSecondPart.size() < 1)
+	{	// in case the input looks strange
+		throw std::invalid_argument(__func__ + std::string(": invalid argument "
+			"(list of values is not in the \"<value>, ... , <value>\" format)"));
+	}
+
+
+	std::vector<unsigned> resultVec;
+	for (std::vector<std::string>::const_iterator itStr
+		= splitSecondPart.begin(); itStr != splitSecondPart.end(); ++itStr)
+	{	// convert all symbols
+		resultVec.push_back(Convert::FromString<unsigned>(*itStr));
+	}
+
+	return ParserResultUnsignedVecType(resultVec,
+		ParseExpressionBoolean(splitInput[0]));
+}
+
+
+std::string FormulaParser::parserResultToString(
+	const ParserResultUnsignedType& prsRes)
+{
+	std::string result = Convert::ToString(prsRes.first);
+
+	for (VariableListType::const_iterator itVars = prsRes.second.begin();
+		itVars != prsRes.second.end(); ++itVars)
+	{	// print all variables
+		result += " * " + Convert::ToString(((itVars->second)? " " : "~"))
+			+ itVars->first;
+	}
+
+	return result;
+}
---
+++ unit_tests/formula_parser.hh
@@ -0,0 +1,200 @@
+/*****************************************************************************
+ *  Vojnar's Army Tree Automata Library
+ *
+ *  Copyright (c) 2011  Ondra Lengal <ilengal@fit.vutbr.cz>
+ *
+ *  Description:
+ *    Header file for FormulaParser; static class that parses formulae.
+ *
+ *****************************************************************************/
+
+#ifndef _VATA_FORMULA_PARSER_HH_
+#define _VATA_FORMULA_PARSER_HH_
+
+// standard library headers
+#include <string>
+#include <vector>
+
+// Boost headers
+#include <boost/algorithm/string.hpp>
+
+
+/**
+ * @brief   Parser of formulae
+ * @author  Ondra Lengal <ilengal@fit.vutbr.cz>
+ * @date    2011
+ *
+ * Static class that is used to parse some types of formulae.
+ */
+class FormulaParser
+{
+public:   // public types
+
+	/**
+	 * @brief  Type for variable occurence
+	 *
+	 * Type that holds name of a variable and information whether it occurs in
+	 * positive or negative.
+	 */
+	typedef std::pair<std::string, bool> VariableOccurenceType;
+
+	/**
+	 * @brief  List of variable occurences
+	 *
+	 * List of variable occurences of type VariableOccurenceType.
+	 */
+	typedef std::vector<VariableOccurenceType> VariableListType;
+
+	/**
+	 * @brief  Parser result type for Boolean
+	 *
+	 * Result type of the parser of formulae with Boolean value (the value is
+	 * always TRUE)
+	 */
+	typedef VariableListType ParserResultBooleanType;
+
+	/**
+	 * @brief  Parser result type for unsigned
+	 *
+	 * Result type of the parser of formulae with unsigned value.
+	 */
+	typedef std::pair<unsigned, VariableListType> ParserResultUnsignedType;
+
+	/**
+	 * @brief  Parser result type for vector of unsigneds
+	 *
+	 * Result type of the parser of formulae with vector of unsigned values.
+	 */
+	typedef std::pair<std::vector<unsigned>, VariableListType>
+		ParserResultUnsignedVecType;
+
+private:  // private methods
+
+	/**
+	 * @brief  Private default constructor
+	 *
+	 * Default constructor which is private to disable creating an instance
+	 * of the class.
+	 */
+	FormulaParser();
+
+
+	/**
+	 * @brief  Private copy constructor
+	 *
+	 * Copy constructor which is private to disable creating an instance
+	 * of the class.
+	 *
+	 * @param[in]  formulaParser  The instance to be copied
+	 */
+	FormulaParser(const FormulaParser& formulaParser);
+
+
+	/**
+	 * @brief  Private assignment operator
+	 *
+	 * Assignment operator which is private to disable creating an instance
+	 * of the class.
+	 *
+	 * @param[in]  formulaParser  The instance to be copied
+	 *
+	 * @returns  The resulting object
+	 */
+	FormulaParser& operator=(const FormulaParser& formulaParser);
+
+
+	/**
+	 * @brief  Private destructor
+	 *
+	 * Private destructor.
+	 */
+	~FormulaParser();
+
+
+public:   // public methods
+
+	/**
+	 * @brief  Parser of formulae with Boolean result for test cases
+	 *
+	 * Parser of formulae with Boolean result for test cases. An example of the
+	 * format of a formula follows:
+	 *                         x1 * x2 * ~x3 * x4
+	 *
+	 * "x1", "x2", "x3", "x4": Boolean variables of the MTBDD (can be arbitrary
+	 *                         strings)
+	 * "~": denotes that the Boolean variable that follows is complemented 
+	 * "*": times symbol
+	 *
+	 * @param[in]  input  The input string
+	 * 
+	 * @returns  Parser result
+	 */
+	static ParserResultBooleanType ParseExpressionBoolean(std::string input);
+
+
+	/**
+	 * @brief  Parser of formulae with unsigned result for test cases
+	 *
+	 * Parser of formulae with unsigned result for test cases. An example of the
+	 * format of a formula follows:
+	 *                       x1 * x2 * ~x3 * x4 = 5
+	 *
+	 * "x1", "x2", "x3", "x4": Boolean variables of the MTBDD (can be arbitrary
+	 *                         strings)
+	 * "~": denotes that the Boolean variable that follows is complemented 
+	 * "*": times symbol
+	 * "5": value that is saved to the leaf
+	 *
+	 * @param[in]  input  The input string
+	 * 
+	 * @returns  Parser result
+	 */
+	static ParserResultUnsignedType ParseExpressionUnsigned(std::string input);
+
+
+	/**
+	 * @brief  Parser of formulae with unsigneds vector result for test cases
+	 *
+	 * Parser of formulae with unsigneds vector result for test cases. An
+	 * example of the format of a formula follows:
+	 *                         x1 * x2 * ~x3 * x4 = { 1 , 2 }
+	 *
+	 * "x1", "x2", "x3", "x4": Boolean variables of the MTBDD (can be arbitrary
+	 *                         strings)
+	 * "~": denotes that the Boolean variable that follows is complemented 
+	 * "*": times symbol
+	 * "{": start of result set
+	 * "}": end of result set
+	 * "1", "2": elements of the result set
+	 *
+	 * @param[in]  input  The input string
+	 * 
+	 * @returns  Parser result
+	 */
+	static ParserResultUnsignedVecType ParseExpressionUnsignedVec(
+		std::string input);
+
+
+	/**
+	 * @brief  Transforms parser result to string
+	 *
+	 * Transforms parser result to string.
+	 *
+	 * @param[in]  prsRes  Parser result
+	 *
+	 * @returns  String that represents the parser result
+	 */
+	static std::string parserResultToString(
+		const ParserResultUnsignedType& prsRes);
+
+
+	/**
+	 * @copybrief  parserResultToString()
+	 *
+	 * @copydetails  parserResultToString()
+	 */
+	static std::string parserResultToString(
+		const ParserResultUnsignedVecType& prsRes);
+};
+
+#endif
