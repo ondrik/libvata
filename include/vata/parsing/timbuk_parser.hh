11d8fb2db0be0091c8eb3eb1f36304acffab6f67
Ondra Lengal <ondra.lengal@gmail.com>
2011-07-26 21:28:59 +0200

    include/vata/parsing/timbuk_parser.hh: add the << operator

--- include/vata/parsing/timbuk_parser.hh
+++ include/vata/parsing/timbuk_parser.hh
@@ -13,6 +13,7 @@
 
 // VATA headers
 #include <vata/vata.hh>
+#include <vata/util/convert.hh>
 #include <vata/util/triple.hh>
 
 namespace VATA
@@ -22,9 +23,6 @@ namespace VATA
 		class TimbukParser;
 	}
 }
-
-
-
 
 
 class VATA::Parsing::TimbukParser
@@ -38,19 +36,19 @@ public:   // data types
 		typedef std::pair<std::string, unsigned> Symbol;
 		typedef std::string State;
 		typedef std::vector<State> StateTuple;
-		typedef VATA::Util::Triple<StateTuple, Symbol, State> Transition;
+		typedef VATA::Util::Triple<StateTuple, std::string, State> Transition;
 
-		typedef std::vector<Symbol> SymbolList;
-		typedef std::vector<State> StateList;
-		typedef std::vector<Transition> TransitionList;
+		typedef std::set<Symbol> SymbolSet;
+		typedef std::set<State> StateSet;
+		typedef std::set<Transition> TransitionSet;
 
 	public:   // data members
 
 		std::string name;
-		SymbolList symbols;
-		StateList states;
-		StateList finalStates;
-		TransitionList transitions;
+		SymbolSet symbols;
+		StateSet states;
+		StateSet finalStates;
+		TransitionSet transitions;
 
 	public:   // methods
 
@@ -61,6 +59,66 @@ public:   // data types
 				finalStates(),
 				transitions()
 		{ }
+
+		bool operator==(const ReturnType& rhs) const;
+
+		friend std::ostream& operator<<(std::ostream& os, const ReturnType& retType)
+		{
+			typedef TimbukParser::ReturnType ReturnType;
+
+			os << "Ops ";
+			for (ReturnType::SymbolSet::const_iterator itSymb = retType.symbols.begin();
+				itSymb != retType.symbols.end(); ++itSymb)
+			{
+				os << itSymb->first << ":" << VATA::Util::Convert::ToString(itSymb->second) << " ";
+			}
+
+			os << "\n";
+			os << "Automaton " << retType.name;
+
+			os << "\n";
+			os << "States ";
+			for (ReturnType::StateSet::const_iterator itSt = retType.states.begin();
+				itSt != retType.states.end(); ++itSt)
+			{
+				os << *itSt << " ";
+			}
+
+			os << "\n";
+			os << "Final States ";
+			for (ReturnType::StateSet::const_iterator itFst = retType.finalStates.begin();
+				itFst != retType.finalStates.end(); ++itFst)
+			{
+				os << *itFst << " ";
+			}
+
+			os << "\n";
+			os << "Transitions\n";
+			for (ReturnType::TransitionSet::const_iterator itTran = retType.transitions.begin();
+				itTran != retType.transitions.end(); ++itTran)
+			{
+				os << itTran->second;
+				if (!(itTran->first.empty()))
+				{
+					os << "(";
+					os << itTran->first[0];
+					for (size_t i = 1; i < itTran->first.size(); ++i)
+					{
+						os << ", ";
+						os << itTran->first[i];
+					}
+
+					os << ")";
+				}
+
+				os << " -> ";
+				os << itTran->third;
+
+				os << "\n";
+			}
+
+			return os;
+		}
 	};
 
 public:   // methods
