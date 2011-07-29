a04172152e6dce571a7ad6566f740588f882fbcf
Ondra Lengal <ondra.lengal@gmail.com>
2011-07-29 13:25:57 +0200

    include/vata/parsing/abstr_parser.hh: add abstract class for automata parsers

---
+++ include/vata/parsing/abstr_parser.hh
@@ -0,0 +1,44 @@
+/*****************************************************************************
+ *  Vojnar's Army Tree Automata Library
+ *
+ *  Copyright (c) 2011  Ondra Lengal <ilengal@fit.vutbr.cz>
+ *
+ *  Description:
+ *    Header file for abstract class of automaton format parser.
+ *
+ *****************************************************************************/
+
+#ifndef _VATA_ABSTR_PARSER_HH_
+#define _VATA_ABSTR_PARSER_HH_
+
+// VATA headers
+#include <vata/vata.hh>
+#include <vata/util/aut_description.hh>
+#include <vata/util/convert.hh>
+#include <vata/util/triple.hh>
+
+
+namespace VATA
+{
+	namespace Parsing
+	{
+		class AbstrParser;
+	}
+}
+
+
+class VATA::Parsing::AbstrParser
+{
+public:   // data types
+
+	typedef VATA::Util::AutDescription AutDescription;
+
+public:   // methods
+
+	virtual AutDescription ParseString(const std::string& str) = 0;
+
+	virtual ~AbstrParser()
+	{ }
+};
+
+#endif
