9a95722a86d8af0bd30ea9f8ffa5ec7037e97e3e
Ondra Lengal <ondra.lengal@gmail.com>
2011-07-29 14:26:27 +0200

    include/vata/serialization/abstr_serializer.hh: add the AbstrSerializer class

---
+++ include/vata/serialization/abstr_serializer.hh
@@ -0,0 +1,41 @@
+/*****************************************************************************
+ *  Vojnar's Army Tree Automata Library
+ *
+ *  Copyright (c) 2011  Ondra Lengal <ilengal@fit.vutbr.cz>
+ *
+ *  Description:
+ *    Header file for abstract serialization class.
+ *
+ *****************************************************************************/
+
+#ifndef _VATA_ABSTR_SERIALIZER_HH_
+#define _VATA_ABSTR_SERIALIZER_HH_
+
+// VATA headers
+#include <vata/vata.hh>
+
+namespace VATA
+{
+	namespace Serialization
+	{
+		class AbstrSerializer;
+	}
+}
+
+
+class VATA::Serialization::AbstrSerializer
+{
+public:   // data types
+
+	typedef VATA::Util::AutDescription AutDescription;
+
+public:   // methods
+
+	virtual std::string Serialize(const AutDescription& desc) = 0;
+
+	virtual ~AbstrSerializer()
+	{ }
+};
+
+
+#endif
