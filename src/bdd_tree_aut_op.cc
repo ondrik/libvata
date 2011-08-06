9ff9b459d8ec2fdd3733cb550f8e7f880dbaaa5a
Ondra Lengal <ondra.lengal@gmail.com>
2011-08-06 22:33:25 +0200

    src/bdd_tree_aut_op.cc: finish intersection
    The intersection for BDD Tree Automata is finished (though not tested
    yet!!!).

--- src/bdd_tree_aut_op.cc
+++ src/bdd_tree_aut_op.cc
@@ -33,8 +33,7 @@ BDDTreeAut VATA::Union<BDDTreeAut>(const BDDTreeAut& lhs, const BDDTreeAut& rhs)
 	typedef std::tr1::unordered_map<StateType, StateType> StateToStateHT;
 
 	class RewriterApplyFunctor :
-		public VATA::MTBDDPkg::AbstractApply1Functor<BDDTreeAut::StateTupleSet,
-		BDDTreeAut::StateTupleSet>
+		public VATA::MTBDDPkg::AbstractApply1Functor<StateTupleSet, StateTupleSet>
 	{
 	private:  // data members
 
@@ -136,10 +135,88 @@ BDDTreeAut VATA::Intersection<BDDTreeAut>(const BDDTreeAut& lhs,
 
 	typedef BDDTreeAut::StateType StateType;
 	typedef BDDTreeAut::StateSet StateSet;
+	typedef BDDTreeAut::StateTuple StateTuple;
+	typedef BDDTreeAut::StateTupleSet StateTupleSet;
 	typedef std::pair<StateType, StateType> StatePair;
 	typedef std::tr1::unordered_map<StatePair, StateType, boost::hash<StatePair> >
 		IntersectionTranslMap;
 	typedef std::map<StateType, StatePair> WorkSetType;
+
+	class IntersectionApplyFunctor :
+		public VATA::MTBDDPkg::AbstractApply2Functor<StateTupleSet, StateTupleSet,
+		StateTupleSet>
+	{
+	private:  // data members
+
+		BDDTreeAut* pResultAut_;
+		IntersectionTranslMap* pTranslMap_;
+		WorkSetType* pWorkset_;
+
+	private:  // methods
+
+		IntersectionApplyFunctor(const IntersectionApplyFunctor&);
+		IntersectionApplyFunctor& operator=(const IntersectionApplyFunctor&);
+
+	public:   // methods
+
+		IntersectionApplyFunctor(BDDTreeAut* pResultAut,
+			IntersectionTranslMap* pTranslMap, WorkSetType* pWorkset) :
+			pResultAut_(pResultAut),
+			pTranslMap_(pTranslMap),
+			pWorkset_(pWorkset)
+		{
+			// Assertions
+			assert(pResultAut != static_cast<BDDTreeAut*>(0));
+			assert(pTranslMap_ != static_cast<IntersectionTranslMap*>(0));
+			assert(pWorkset_ != static_cast<WorkSetType*>(0));
+		}
+
+		virtual StateTupleSet ApplyOperation(const StateTupleSet& lhs,
+			const StateTupleSet& rhs)
+		{
+			// Assertions
+			assert(pResultAut_ != static_cast<BDDTreeAut*>(0));
+			assert(pTranslMap_ != static_cast<IntersectionTranslMap*>(0));
+			assert(pWorkset_ != static_cast<WorkSetType*>(0));
+
+			StateTupleSet result;
+
+			for (StateTupleSet::const_iterator itLhs = lhs.begin();
+				itLhs != lhs.end(); ++itLhs)
+			{	// for each tuple from LHS
+				for (StateTupleSet::const_iterator itRhs = rhs.begin();
+					itRhs != rhs.end(); ++itRhs)
+				{	// for each tuple from RHS
+					assert(itLhs->size() == itRhs->size());
+
+					StateTuple resultTuple;
+					for (size_t i = 0; i < itLhs->size(); ++i)
+					{	// for each position in the tuples
+						StatePair newPair = std::make_pair((*itLhs)[i], (*itRhs)[i]);
+
+						StateType state;
+						IntersectionTranslMap::const_iterator itTransl;
+						if ((itTransl = pTranslMap_->find(newPair)) != pTranslMap_->end())
+						{	// if the pair is already known
+							state = itTransl->second;
+						}
+						else
+						{	// if the pair is new
+							state = pResultAut_->AddState();
+							pTranslMap_->insert(std::make_pair(newPair, state));
+							pWorkset_->insert(std::make_pair(state, newPair));
+						}
+
+						resultTuple.push_back(state);
+					}
+
+					result.insert(resultTuple);
+				}
+			}
+
+			return result;
+		}
+	};
 
 	BDDTreeAut result;
 	WorkSetType workset;
@@ -161,11 +238,17 @@ BDDTreeAut VATA::Intersection<BDDTreeAut>(const BDDTreeAut& lhs,
 		}
 	}
 
+	IntersectionApplyFunctor isect(&result, &translMap, &workset);
+
 	while (!workset.empty())
 	{	// while there is something in the workset
 		const StatePair& procPair  = workset.begin()->second;
 		const StateType& procState = workset.begin()->first;
 
+		BDDTreeAut::TransMTBDD mtbdd = isect(lhs.getMtbdd(procPair.first),
+			rhs.getMtbdd(procPair.second));
+
+		result.setMtbdd(procState, mtbdd);
 
 		// remove the processed state from the workset
 		workset.erase(workset.begin());
