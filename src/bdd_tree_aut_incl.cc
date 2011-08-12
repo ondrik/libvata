cee58ea01e27b7a19cf9aa264e299f6abdf178de
Ondra Lengal <ondra.lengal@gmail.com>
2011-08-12 19:36:55 +0200

    bdd_tree_aut_incl.cc: add stub of inclusion

---
+++ src/bdd_tree_aut_incl.cc
@@ -0,0 +1,27 @@
+/*****************************************************************************
+ *  Vojnar's Army Tree Automata Library
+ *
+ *  Copyright (c) 2011  Ondra Lengal <ilengal@fit.vutbr.cz>
+ *
+ *  Description:
+ *    Implementation of inclusion on BDD tree automata
+ *
+ *****************************************************************************/
+
+// VATA headers
+#include <vata/vata.hh>
+#include <vata/bdd_tree_aut.hh>
+#include <vata/bdd_tree_aut_op.hh>
+#include <vata/tree_incl_down.hh>
+
+using VATA::BDDTreeAut;
+
+template <>
+bool VATA::CheckInclusion(const BDDTreeAut& smaller, const BDDTreeAut& bigger)
+{
+	// Assertions
+	assert(smaller.isValid());
+	assert(bigger.isValid());
+
+	return CheckDownwardTreeInclusion(smaller, bigger);
+}
