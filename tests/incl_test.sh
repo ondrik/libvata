f97720412661dd9e22c623670355097528b936e5
Ondra Lengal <ondra.lengal@gmail.com>
2011-08-19 18:59:23 +0200

    tests/incl_test.sh: add stub for inclusion test cases

---
+++ tests/incl_test.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+################################# CONSTANTS ##################################
+
+# default timeout (can be overriden from command line)
+timeout=300;
+
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=`readlink -f $0`
+
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=`dirname $SCRIPT`
+
+METHODS=("suif" "suuf" "suus" "sdif" "sddf" "sdds" "seif" "sedf" "seds" "symsu" "symsd" "symsds" "symsdsw" "symsdsb" "symsdsbw");
+
+################################# FUNCTIONS ##################################
+
+# Function that terminates the script with a message
+function die {
+  echo "$1";
+  exit -1;
+}
+
+# Function that runs a single test
+# takes:   <LHS automaton>   <RHS automaton>  <method>   <file to write result>
+function runone {
+  ulimit -St "$timeout";
+  ${SCRIPTPATH}/incl_wrapper.sh "$1" "$2" "$3" &> "$4"
+  return $?
+}
+
+# Function that runs all tests for one pair of automata
+function runall {
+  local res="?";
+  local i;
+
+  # run all methods
+  for (( i=0; i < ${#METHODS[*]}; i++ )); do
+    runone "${METHODS[$i]}" "$1" "$2" "${tmp[$i]}" &
+    pid[$i]=$!;
+  done;
+
+  # evaluate all methods
+  for (( i=0; i < ${#METHODS[*]}; i++ )); do
+    wait ${pid[$i]};
+    local ret=$?;
+    if [ "$ret" == "152" ]; then
+      printcolumn "-"
+    else
+      local v=(`<"${tmp[$i]}"`);
+      [ "$ret" == "0" ] || die "inclusion test failed! ($ret)"; 
+      if [ "$res" == "?" ]; then
+        res=${v[0]};
+      else
+        [ "$res" == ${v[0]} ] || die "inconsistent results! ($res != ${v[0]})";
+      fi;
+      printcolumn $(printf "%.4f" "${v[1]}");
+    fi;
+  done;
+  printcolumn "$res"
+  printf "\n"
+}
+
+# Prints a column into the result table
+function printcolumn {
+  printf "%10s;" "$1"
+}
+
+################################## PROGRAM ###################################
+
+# Check the number of command-line arguments
+if [ \( "$#" -eq 0 \) -o \( "$#" -gt 2 \) ] ; then
+  die "usage: $0 <dir> [timeout]"
+fi 
+
+# If timeout is specified, use it instead of the default
+if [ "$2" != "" ]; then
+  timeout="$2";
+fi;
+
+AUT_DIR="$1"
+
+# If the directory doesn't exist, terminate
+if [ ! -d "${AUT_DIR}" ]; then
+  die "directory '${AUT_DIR}' does not exist!"
+fi
+
+echo "======== Checking inclusion of automata ======="
+echo "Automata directory:   ${1}"
+echo "Timeout:              ${timeout}"
+
+# Create temporary files for storing the output
+for (( i=0; i < ${#METHODS[*]}; i++ )); do
+  tmp[$i]=`mktemp`;
+done;
+
+echo "reading files ..."
+cnt=0;
+for x in $1/*; do
+  key[$cnt]=`basename $x`;
+  #val[$cnt]=`./main n <"$x"`;
+  val[$cnt]="${x}";
+  #[ "$?" == "0" ] || die "unable to preprocess $x";
+  #printf "$x (%s)\n" "`./main i <<< ${val[$cnt]}`";
+  cnt=$(($cnt+1));
+done;
+
+echo "-----------------------------------------------"
+for i in "aut1" "aut2" ; do
+  printcolumn ${i}
+done
+
+for (( i=0; i < ${#METHODS[*]}; i++ )); do
+  printcolumn "${METHODS[$i]}"
+done;
+
+printcolumn "'<='"
+printf "\n"
+
+for (( i=0; i < $cnt; i++ )); do
+  for (( j=0; j < $cnt; j++ )); do
+    printcolumn ${key[$i]}
+    printcolumn ${key[$j]}
+    runall "${val[$i]}" "${val[$j]}";
+  done;
+done;
+
---
+++ tests/incl_wrapper.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo 0
+echo 0.00001
