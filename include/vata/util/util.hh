84735b4446b88db1697a84698fe15c19f86ca17d
Ondra Lengal <ondra.lengal@gmail.com>
2011-08-20 19:39:56 +0200

    util.hh: add header file for misc gadgets

---
+++ include/vata/util/util.hh
@@ -0,0 +1,25 @@
+/*****************************************************************************
+ *  Vojnar's Army Tree Automata Library
+ *
+ *  Copyright (c) 2011  Ondra Lengal <ilengal@fit.vutbr.cz>
+ *
+ *  Description:
+ *    Header file for miscellaneous utility gadgets.
+ *
+ *****************************************************************************/
+
+#ifndef _VATA_UTIL_HH_
+#define _VATA_UTIL_HH_
+
+// VATA headers
+#include <vata/vata.hh>
+
+namespace VATA
+{
+	namespace Util
+	{
+		std::string ReadFile(const std::string& fileName);
+	}
+}
+
+#endif
---
+++ src/util.cc
@@ -0,0 +1,31 @@
+/*****************************************************************************
+ *  Vojnar's Army Tree Automata Library
+ *
+ *  Copyright (c) 2011  Ondra Lengal <ilengal@fit.vutbr.cz>
+ *
+ *  Description:
+ *    Implementation of miscellaneous utility gadgets.
+ *
+ *****************************************************************************/
+
+// VATA headers
+#include <vata/vata.hh>
+#include <vata/util/util.hh>
+
+// Standard library headers
+#include <fstream>
+
+std::string VATA::Util::ReadFile(const std::string& fileName)
+{
+	std::ifstream t(fileName);
+	std::string str;
+
+	t.seekg(0, std::ios::end);
+	str.reserve(t.tellg());
+	t.seekg(0, std::ios::beg);
+
+	str.assign((std::istreambuf_iterator<char>(t)),
+		std::istreambuf_iterator<char>());
+
+	return str;
+}
