9e394cb3d50942e8e32ef6606229119dbc827eec
Jiri Simacek <isimacek@fit.vutbr.cz>
2011-09-20 13:43:49 +0200

    explicit_tree_aut_op.hh: automata intersection

--- include/vata/explicit_tree_aut_op.hh
+++ include/vata/explicit_tree_aut_op.hh
@@ -48,11 +48,99 @@ namespace VATA {
 
 	template <class SymbolType>
 	ExplicitTreeAut<SymbolType> Intersection(
-		const ExplicitTreeAut<SymbolType>& /* lhs */,
-		const ExplicitTreeAut<SymbolType>& /* rhs */,
-		AutBase::ProductTranslMap* /* pTranslMap */ = nullptr)
-	{
-		throw std::runtime_error("Unimplemented");
+		const ExplicitTreeAut<SymbolType>& lhs,
+		const ExplicitTreeAut<SymbolType>& rhs,
+		AutBase::ProductTranslMap* pTranslMap = nullptr) {
+
+		typedef ExplicitTreeAut<SymbolType> ExplicitTA;
+
+		AutBase::ProductTranslMap translMap;
+
+		if (!pTranslMap)
+			pTranslMap = &translMap;
+
+		ExplicitTreeAut<SymbolType> res(lhs.cache_);
+
+		std::vector<const AutBase::ProductTranslMap::value_type*> stack;
+
+		for (auto s : lhs.finalStates_) {
+
+			for (auto t : rhs.finalStates_) {
+
+				auto u = pTranslMap->insert(
+					std::make_pair(std::make_pair(s, t), pTranslMap->size())
+				).first;
+
+				res.SetStateFinal(u->second);
+
+				stack.push_back(&*u);
+
+			}
+
+		}
+
+		while (!stack.empty()) {
+
+			auto p = stack.back();
+
+			stack.pop_back();
+
+			auto leftCluster = ExplicitTA::genericLookup(*lhs.transitions_, p->first.first);
+
+			if (!leftCluster)
+				continue;
+
+			auto rightCluster = ExplicitTA::genericLookup(*rhs.transitions_, p->first.second);
+
+			if (!rightCluster)
+				continue;
+
+			assert(leftCluster);
+
+			for (auto leftSymbolStateTupleSetPtr : *leftCluster) {
+
+				auto rightTupleSet =
+					ExplicitTA::genericLookup(*rightCluster, leftSymbolStateTupleSetPtr.first);
+
+				if (!rightTupleSet)
+					continue;
+
+				for (auto leftTuplePtr : *leftSymbolStateTupleSetPtr.second) {
+
+					for (auto rightTuplePtr : *rightTupleSet) {
+
+						assert(leftTuplePtr->size() == rightTuplePtr->size());
+
+						typename ExplicitTA::StateTuple children;
+
+						for (size_t i = 0; i < leftTuplePtr->size(); ++i) {
+
+							auto u = pTranslMap->insert(
+								std::make_pair(
+									std::make_pair((*leftTuplePtr)[i], (*rightTuplePtr)[i]),
+									pTranslMap->size()
+								)
+							);
+
+							if (u.second)
+								stack.push_back(&*u.first);
+
+							children.push_back(u.first->second);
+
+						}
+
+						res.AddTransition(children, leftSymbolStateTupleSetPtr.first, p->second);
+
+					}					
+					
+				}
+
+			}
+
+		}
+
+		return res;
+
 	}
 
 	template <class SymbolType>
@@ -131,7 +219,7 @@ namespace VATA {
 
 		result.finalStates_ = aut.finalStates_;
 		result.transitions_ = StateToTransitionClusterMapPtr(
-				new typename ExplicitTA::StateToTransitionClusterMap()
+			new typename ExplicitTA::StateToTransitionClusterMap()
 		);
 
 		for (auto state : reachableStates) {
