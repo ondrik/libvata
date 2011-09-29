930f5a2e1d0eaba4be84257b71853e1420d02309
Ondra Lengal <ondra.lengal@gmail.com>
2011-09-29 12:50:33 +0200

    unit_tests/tree_aut_test.hh: sanitize the test

--- unit_tests/tree_aut_test.hh
+++ unit_tests/tree_aut_test.hh
@@ -467,10 +467,7 @@ BOOST_AUTO_TEST_CASE(aut_remove_useless)
 		readAut(aut, stateDict, autStr);
 		AutDescription autDesc = parser_.ParseString(autStr);
 
-		StateToStateMap translMap;
 		AutType autNoUseless = VATA::RemoveUselessStates(aut);
-		translMap.clear();
-		autNoUseless = VATA::RemoveUnreachableStates(autNoUseless);
 		std::string autNoUselessStr = dumpAut(autNoUseless, stateDict);
 
 		AutDescription descOutNoUseless = parser_.ParseString(autNoUselessStr);
