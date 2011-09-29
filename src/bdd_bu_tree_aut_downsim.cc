df3ce4f949ca5b38c2d5c44e08c814787bc75ee7
Ondra Lengal <ondra.lengal@gmail.com>
2011-09-29 17:59:03 +0200

    bdd_bu_tree_aut_downsim.cc: add something that can work

--- src/bdd_bu_tree_aut_downsim.cc
+++ src/bdd_bu_tree_aut_downsim.cc
@@ -45,7 +45,7 @@ typedef std::stack<RemoveElement, std::list<RemoveElement>> RemoveList;
 typedef BDDBottomUpTreeAut::TransTable BUTransTable;
 
 template <class ArbitraryFunction>
-void forAllTuplesWithMatchingStatesDo(
+static void forAllTuplesWithMatchingStatesDo(
 	const BUTransTable& tuples, const StateType& lhsState,
 	const StateType& rhsState, ArbitraryFunction func)
 {
@@ -71,7 +71,7 @@ void forAllTuplesWithMatchingStatesDo(
 		{
 			const StateTuple& rhsTuple = rhsTupleBddPair.first;
 
-			if (rhsTuple.size() != rhsTuple.size())
+			if (rhsTuple.size() != lhsTuple.size())
 			{
 				continue;
 			}
@@ -79,8 +79,7 @@ void forAllTuplesWithMatchingStatesDo(
 			size_t i;
 			for (i = 0; i < matchedPositions.size(); ++i)
 			{
-				if ((rhsTuple[matchedPositions[i]] == lhsState) &&
-					(rhsTuple[matchedPositions[i]] == rhsState))
+				if (rhsTuple[matchedPositions[i]] == rhsState)
 				{
 					break;
 				}
@@ -96,7 +95,7 @@ void forAllTuplesWithMatchingStatesDo(
 	}
 }
 
-inline bool componentWiseSim(const StateBinaryRelation& sim,
+static inline bool componentWiseSim(const StateBinaryRelation& sim,
 	const StateTuple& lhsTuple, const StateTuple& rhsTuple)
 {
 	for (size_t i = 0; i < lhsTuple.size(); ++i)
@@ -111,147 +110,148 @@ inline bool componentWiseSim(const StateBinaryRelation& sim,
 }
 
 
-GCC_DIAG_OFF(effc++)
-class InitCntApplyFctor :
-	public VATA::MTBDDPkg::Apply2Functor<InitCntApplyFctor, StateTupleSet,
-	CounterElementMap, CounterElementMap>
+namespace
 {
-GCC_DIAG_ON(effc++)
-
-private:  // data members
-
-	const StateType& state_;
-
-public:   // methods
-
-	InitCntApplyFctor(const StateType& state) :
-		state_(state)
-	{ }
-
-	CounterElementMap ApplyOperation(const StateTupleSet& lhs,
-		const CounterElementMap& rhs)
+	GCC_DIAG_OFF(effc++)
+	class InitCntApplyFctor :
+		public VATA::MTBDDPkg::Apply2Functor<InitCntApplyFctor, StateTupleSet,
+		CounterElementMap, CounterElementMap>
 	{
-		CounterElementMap result = rhs;
-		if (!result.insert(std::make_pair(state_, lhs.size())).second)
+	GCC_DIAG_ON(effc++)
+
+	private:  // data members
+
+		const StateType& state_;
+
+	public:   // methods
+
+		InitCntApplyFctor(const StateType& state) :
+			state_(state)
+		{ }
+
+		CounterElementMap ApplyOperation(const StateTupleSet& lhs,
+			const CounterElementMap& rhs)
 		{
-			assert(false);
-		}
-
-		return result;
-	}
-};
-
-
-GCC_DIAG_OFF(effc++)
-class InitRefineApplyFctor :
-	public VATA::MTBDDPkg::VoidApply2Functor<InitRefineApplyFctor, StateTupleSet,
-	StateTupleSet>
-{
-GCC_DIAG_ON(effc++)
-
-private:  // data members
-
-	bool& isSim_;
-
-public:   // methods
-
-	InitRefineApplyFctor(bool& isSim) :
-		isSim_(isSim)
-	{ }
-
-	void ApplyOperation(const StateTupleSet& lhs, const StateTupleSet& rhs)
-	{
-		if (!lhs.empty() && rhs.empty())
-		{
-			isSim_ = false;
-			stopProcessing();
-		}
-	}
-};
-
-GCC_DIAG_OFF(effc++)
-class RefineApplyFctor :
-	public VATA::MTBDDPkg::Apply3Functor<RefineApplyFctor, StateSet, StateSet,
-	CounterElementMap, CounterElementMap>
-{
-GCC_DIAG_ON(effc++)
-
-private:  // data members
-
-	const BUTransTable& transTable_;
-	StateBinaryRelation& sim_;
-	RemoveList& remove_;
-
-public:   // methods
-
-	RefineApplyFctor(const BUTransTable& transTable, StateBinaryRelation& sim,
-		RemoveList& remove) :
-		transTable_(transTable),
-		sim_(sim),
-		remove_(remove)
-	{ }
-
-	CounterElementMap ApplyOperation(const StateSet& upR, const StateSet& upQ,
-		const CounterElementMap& cntQ)
-	{
-		if (upR.empty())
-		{
-			return cntQ;
-		}
-
-		VATA_LOGGER_INFO("upR = " + Convert::ToString(upR) + ", upQ = " + Convert::ToString(upQ) + ", cntQ = " + Convert::ToString(cntQ));
-		CounterElementMap result = cntQ;
-
-		for (const StateType& s : upR)
-		{
-			CounterElementMap::iterator itCnt;
-			if ((itCnt = result.find(s)) == result.end())
+			CounterElementMap result = rhs;
+			if (!result.insert(std::make_pair(state_, lhs.size())).second)
 			{
-				assert(false);    // fail gracefully
+				assert(false);
 			}
 
-			if (--(itCnt->second) == 0)
-			{
-				for (const StateType& p : upQ)
-				{
-					if (sim_.get(p, s))
-					{	// if 's' simulates 'p'
-						forAllTuplesWithMatchingStatesDo(
-							transTable_, p, s,
-							[this](const StateTuple& pTuple, const StateTuple& sTuple){
-								if (componentWiseSim(sim_, pTuple, sTuple))
-								{
-									remove_.push(std::make_pair(pTuple, sTuple));
-								}}
-							);
+			return result;
+		}
+	};
 
-						// 's' no longer simulates 'p'
-						sim_.set(p, s, false);
+
+	GCC_DIAG_OFF(effc++)
+	class InitRefineApplyFctor :
+		public VATA::MTBDDPkg::VoidApply2Functor<InitRefineApplyFctor, StateTupleSet,
+		StateTupleSet>
+	{
+	GCC_DIAG_ON(effc++)
+
+	private:  // data members
+
+		bool& isSim_;
+
+	public:   // methods
+
+		InitRefineApplyFctor(bool& isSim) :
+			isSim_(isSim)
+		{ }
+
+		void ApplyOperation(const StateTupleSet& lhs, const StateTupleSet& rhs)
+		{
+			if (!lhs.empty() && rhs.empty())
+			{
+				isSim_ = false;
+				stopProcessing();
+			}
+		}
+	};
+
+	GCC_DIAG_OFF(effc++)
+	class RefineApplyFctor :
+		public VATA::MTBDDPkg::Apply3Functor<RefineApplyFctor, StateSet, StateSet,
+		CounterElementMap, CounterElementMap>
+	{
+	GCC_DIAG_ON(effc++)
+
+	private:  // data members
+
+		const BUTransTable& transTable_;
+		StateBinaryRelation& sim_;
+		RemoveList& remove_;
+
+	public:   // methods
+
+		RefineApplyFctor(const BUTransTable& transTable, StateBinaryRelation& sim,
+			RemoveList& remove) :
+			transTable_(transTable),
+			sim_(sim),
+			remove_(remove)
+		{ }
+
+		CounterElementMap ApplyOperation(const StateSet& upR, const StateSet& upQ,
+			const CounterElementMap& cntQ)
+		{
+			if (upR.empty())
+			{
+				return cntQ;
+			}
+
+			CounterElementMap result = cntQ;
+
+			for (const StateType& s : upR)
+			{
+				CounterElementMap::iterator itCnt;
+				if ((itCnt = result.find(s)) == result.end())
+				{
+					assert(false);    // fail gracefully
+				}
+
+				if (--(itCnt->second) == 0)
+				{
+					for (const StateType& p : upQ)
+					{
+						if (sim_.get(p, s))
+						{	// if 's' simulates 'p'
+							forAllTuplesWithMatchingStatesDo(
+								transTable_, p, s,
+								[this](const StateTuple& pTuple, const StateTuple& sTuple){
+									if (componentWiseSim(sim_, pTuple, sTuple))
+									{
+										remove_.push(std::make_pair(pTuple, sTuple));
+									}}
+								);
+
+							// 's' no longer simulates 'p'
+							sim_.set(p, s, false);
+						}
 					}
+
 				}
 
 			}
 
+			return cntQ;
 		}
 
-		return cntQ;
-	}
-
-};
-
+	};
+}
 
 
 StateBinaryRelation VATA::ComputeDownwardSimulation(
 	const BDDBottomUpTreeAut& aut)
 {
-	BDDBottomUpTreeAut newAut;
-
 	StateType stateCnt = 0;
 	StateToStateMap stateMap;
 	StateToStateTranslator stateTrans(stateMap,
 		[&stateCnt](const StateType&){return stateCnt++;});
 
-	aut.ReindexStates(newAut, stateTrans);
+	BDDBottomUpTreeAut newAut = RemoveUselessStates(aut);
+	BDDBottomUpTreeAut reindexedAut;
+	newAut.ReindexStates(reindexedAut, stateTrans);
 
 	return ComputeDownwardSimulation(newAut, stateCnt);
 }
