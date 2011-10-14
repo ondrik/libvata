60dd5eb888e5d18e5a1c514ce3e77339b2797d0c
Jiri Simacek <isimacek@fit.vutbr.cz>
2011-10-14 17:31:00 +0200

    explicit bottom-up inclusion imported from ultimate-bu-inclusion branch

--- include/vata/explicit_tree_incl_up.hh
+++ include/vata/explicit_tree_incl_up.hh
@@ -11,6 +11,7 @@
 #ifndef _VATA_EXPLICIT_TREE_INCL_UP_HH_
 #define _VATA_EXPLICIT_TREE_INCL_UP_HH_
 
+#include <set>
 #include <algorithm>
 
 #include <boost/functional/hash.hpp>
@@ -19,6 +20,7 @@
 #include <vata/util/cached_binary_op.hh>
 #include <vata/util/antichain1c.hh>
 #include <vata/util/antichain2c_v2.hh>
+#include <vata/util/ordered_antichain2c.hh>
 
 //#include <vata/vata.hh>
 //#include <vata/util/convert.hh>
@@ -84,6 +86,32 @@ public:
 		typedef typename Util::Antichain1C<SmallerType> Antichain1C;
 		typedef typename Util::Antichain2Cv2<SmallerType, BiggerType> Antichain2C;
 
+		typedef std::pair<SmallerType, BiggerType> SmallerBiggerPair;
+
+		struct less {
+
+			bool operator()(const SmallerBiggerPair& p1, const SmallerBiggerPair& p2) const {
+
+				if (p1.second->size() < p2.second->size())
+					return true;
+
+				if (p1.second->size() > p2.second->size())
+					return false;
+
+				if (p1.first < p2.first)
+					return true;
+
+				if (p1.first > p2.first)
+					return false;
+
+				return p1.second.get() < p2.second.get();
+
+			}
+
+		};
+
+		typedef typename Util::OrderedAntichain2C<Antichain2C, less> OrderedAntichain2C;
+
 		GCC_DIAG_OFF(effc++)
 		struct Choice {
 		GCC_DIAG_ON(effc++)
@@ -95,8 +123,6 @@ public:
 
 				if (!biggerList)
 					return false;
-
-				assert(biggerList->size());
 
 				this->biggerList_ = biggerList;
 				this->current_ = biggerList->begin();
@@ -116,29 +142,40 @@ public:
 			}
 
 			const BiggerType& get() const { return *this->current_; }
-			
+
 		};
 
-		GCC_DIAG_OFF(effc++)
-		struct ChoiceVector : public std::vector<Choice> {
-		GCC_DIAG_ON(effc++)
+		struct ChoiceVector {
 
 			const Antichain2C& processed_;
+			const Antichain2C::TList& fixed_;
+			std::vector<Choice> state_;
 			
 		public:
 
-			ChoiceVector(const Antichain2C& processed)
-				: std::vector<Choice>(), processed_(processed) {}
+			ChoiceVector(const Antichain2C& processed,
+				const Antichain2C::TList& fixed)
+				: processed_(processed), fixed_(fixed), state_() {}
 		
-			bool build(const Explicit::StateTuple& children) {
+			bool build(const Explicit::StateTuple& children, size_t index) {
 
-				assert(children.size());
+				assert(index < children.size());
 
-				this->resize(children.size());
+				this->state_.resize(children.size());
 
-				for (size_t i = 0; i < this->size(); ++i) {
+				for (size_t i = 0; i < index; ++i) {
 
-					if (!(*this)[i].init(this->processed_.lookup(children[i])))
+					if (!this->state_[i].init(this->processed_.lookup(children[i])))
+						return false;
+
+				}
+
+				this->state_[index].biggerList_ = &this->fixed_;
+				this->state_[index].current_ = this->fixed_.begin();
+
+				for (size_t i = index + 1; i < children.size(); ++i) {
+
+					if (!this->state_[i].init(this->processed_.lookup(children[i])))
 						return false;
 
 				}
@@ -146,10 +183,10 @@ public:
 				return true;
 
 			}
-
+		
 			bool next() {
 
-				for (auto& choice : *this) {
+				for (auto& choice : this->state_) {
 
 					if (choice.next())
 						return true;
@@ -157,6 +194,12 @@ public:
 				}
 
 				return false;
+
+			}
+
+			const std::vector<Choice>& state() const {
+
+				return this->state_;
 
 			}
 
@@ -195,9 +238,9 @@ public:
 		
 		auto gte = [&lte](const BiggerType& x, const BiggerType& y) { return lte(y, x); };
 
-		typename Aut::SymbolToTransitionListMap smallerLeaves, biggerLeaves;
-		typename Aut::IndexedSymbolToTransitionListMap smallerIndex;
+		typename Aut::IndexedSymbolToIndexedTransitionListMap smallerIndex;
 		typename Aut::SymbolToDoubleIndexedTransitionListMap biggerIndex;
+		typename Aut::SymbolToTransitionListMap smallerLeaves, biggerLeaves;
 
 		smaller.bottomUpIndex(smallerIndex, smallerLeaves);
 		bigger.bottomUpIndex(biggerIndex, biggerLeaves);
@@ -256,10 +299,10 @@ public:
 			}
 		);
 
-		std::unordered_set<SmallerType> active;
-
 		Antichain1C post;
-		Antichain2C next, processed;
+
+		Antichain2C temporary, processed;
+		OrderedAntichain2C next;
 
 		bool isAccepting;
 
@@ -319,19 +362,23 @@ public:
 				processed.refine(inv[transition->state()], ptr, gte);
 				processed.insert(transition->state(), ptr);
 
-				active.insert(transition->state());
+				next.insert(transition->state(), ptr);
 
 			}
 
 		}
 
-		ChoiceVector choiceVector(processed);
+		SmallerType q;
 
-		while (!active.empty()) {
+		Antichain2C::TList fixedList(1);
 
-			SmallerType q = *active.begin();
+		BiggerType& Q = fixedList.front();
 
-			active.erase(active.begin());
+		ChoiceVector choiceVector(processed, fixedList);
+
+		while (next.get(q, Q)) {
+
+			assert(q < inv.size());
 
 			// Post(processed)
 
@@ -339,120 +386,121 @@ public:
 
 				assert(symbolClusterPair.second.size() > 0);
 
-				auto biggerClusterIter = biggerIndex.find(symbolClusterPair.first);
+				size_t i = 0;
 
-				if (biggerClusterIter == biggerIndex.end())
-					return false;
+				for (auto& smallerTransitions : symbolClusterPair.second) {
 
-				for (auto& smallerTransition : symbolClusterPair.second) {
-
-					assert(smallerTransition);
-
-					if (!choiceVector.build(smallerTransition->children()))
-						continue;
-
-					do {
-
-						post.clear();
-						isAccepting = false;
-
-						assert(biggerClusterIter->second.size() > 0);
-						assert(choiceVector[0].get());
-
-						auto firstSet = evalTransitions(
-							symbolClusterPair.first, 0, choiceVector[0].get().get()
-						);
-
-						assert(firstSet);
-
-						std::list<const Transition*> biggerTransitions(
-							firstSet->begin(), firstSet->end()
-						);
-
-						for (size_t i = 1; i < choiceVector.size(); ++i) {
-
-							assert(i < biggerClusterIter->second.size());
-							assert(choiceVector[i].get());
-			
-							auto transitions = evalTransitions(
-								symbolClusterPair.first, i, choiceVector[i].get().get()
+					for (auto& smallerTransition : smallerTransitions) {
+	
+						assert(smallerTransition);
+	
+						if (!choiceVector.build(smallerTransition->children(), i))
+							continue;
+	
+						do {
+	
+							post.clear();
+							isAccepting = false;
+	
+							assert(choiceVector.state()[0].get());
+	
+							auto firstSet = evalTransitions(
+								symbolClusterPair.first, 0, choiceVector.state()[0].get().get()
 							);
-
-							assert(transitions);
+	
+							assert(firstSet);
+	
+							std::list<const Transition*> biggerTransitions(
+								firstSet->begin(), firstSet->end()
+							);
+	
+							for (size_t i = 1; i < choiceVector.state().size(); ++i) {
+	
+								assert(choiceVector.state()[i].get());
+				
+								auto transitions = evalTransitions(
+									symbolClusterPair.first, i, choiceVector.state()[i].get().get()
+								);
+	
+								assert(transitions);
+								
+								intersectionByLookup(biggerTransitions, *transitions);
+	
+							}
+	
+							for (auto& biggerTransition : biggerTransitions) {
+	
+								assert(biggerTransition);
+								assert(biggerTransition->state() < ind.size());
+	
+								if (post.contains(ind[biggerTransition->state()]))
+									continue;
+	
+								assert(biggerTransition->state() < inv.size());
+	
+								post.refine(inv[biggerTransition->state()]);
+								post.insert(biggerTransition->state());
+								
+								isAccepting = isAccepting ||
+									bigger.IsFinalState(biggerTransition->state());
+	
+							}
 							
-							intersectionByLookup(biggerTransitions, *transitions);
-
-						}
-
-						for (auto& biggerTransition : biggerTransitions) {
-
-							assert(biggerTransition);
-							assert(biggerTransition->state() < ind.size());
-
-							if (post.contains(ind[biggerTransition->state()]))
-								continue;
-
-							assert(biggerTransition->state() < inv.size());
-
-							post.refine(inv[biggerTransition->state()]);
-							post.insert(biggerTransition->state());
-							
-							isAccepting = isAccepting ||
-								bigger.IsFinalState(biggerTransition->state());
-
-						}
-						
-						if (post.data().empty())
-							return false;
-
-						if (!isAccepting && smaller.IsFinalState(smallerTransition->state()))
-							return false;
+							if (post.data().empty())
+								return false;
 	
-						StateSet tmp(post.data().begin(), post.data().end());
-
-						std::sort(tmp.begin(), tmp.end());
-						
-						assert(smallerTransition->state() < ind.size());
-	
-						if (checkIntersection(ind[smallerTransition->state()], tmp))
-							continue;
-
-						auto ptr = biggerTypeCache.lookup(tmp);
-	
-						if (next.contains(ind[smallerTransition->state()], ptr, lte))
-							continue;
+							if (!isAccepting && smaller.IsFinalState(smallerTransition->state()))
+								return false;
 		
-						assert(smallerTransition->state() < inv.size());
+							StateSet tmp(post.data().begin(), post.data().end());
 	
-						next.refine(inv[smallerTransition->state()], ptr, gte);
-						next.insert(smallerTransition->state(), ptr);
-
-					} while (choiceVector.next());
-
-					for (auto& smallerBiggerListPair : next.data()) {
-
-						for (auto& bigger : smallerBiggerListPair.second) {
-
-							assert(smallerBiggerListPair.first < ind.size());
+							std::sort(tmp.begin(), tmp.end());
+							
+							assert(smallerTransition->state() < ind.size());
+		
+							if (checkIntersection(ind[smallerTransition->state()], tmp))
+								continue;
 	
-							if (processed.contains(ind[smallerBiggerListPair.first], bigger, lte))
+							auto ptr = biggerTypeCache.lookup(tmp);
+		
+							if (temporary.contains(ind[smallerTransition->state()], ptr, lte))
 								continue;
 			
-							assert(smallerBiggerListPair.first < inv.size());
+							assert(smallerTransition->state() < inv.size());
+		
+							temporary.refine(inv[smallerTransition->state()], ptr, gte);
+							temporary.insert(smallerTransition->state(), ptr);
 	
-							processed.refine(inv[smallerBiggerListPair.first], bigger, gte);
-							processed.insert(smallerBiggerListPair.first, bigger);
+						} while (choiceVector.next());
 	
-							active.insert(smallerBiggerListPair.first);
-							
+						for (auto& smallerBiggerListPair : temporary.data()) {
+	
+							for (auto& bigger : smallerBiggerListPair.second) {
+	
+								assert(smallerBiggerListPair.first < ind.size());
+		
+								if (processed.contains(ind[smallerBiggerListPair.first], bigger, lte))
+									continue;
+				
+								assert(smallerBiggerListPair.first < inv.size());
+		
+								processed.refine(inv[smallerBiggerListPair.first], bigger, gte);
+								processed.insert(smallerBiggerListPair.first, bigger);
+		
+								next.insert(smallerBiggerListPair.first, bigger);
+								
+							}
+	
 						}
-
+	
+						temporary.clear();
+	
 					}
 
-					next.clear();
+					++i;
 
 				}
-
+	
 			}
 
 		}
@@ -474,6 +522,32 @@ public:
 
 		typedef typename Util::Antichain1C<SmallerType> Antichain1C;
 		typedef typename Util::Antichain2Cv2<SmallerType, BiggerType> Antichain2C;
+
+		typedef std::pair<SmallerType, BiggerType> SmallerBiggerPair;
+
+		struct less {
+
+			bool operator()(const SmallerBiggerPair& p1, const SmallerBiggerPair& p2) const {
+
+				if (p1.second->size() < p2.second->size())
+					return true;
+
+				if (p1.second->size() > p2.second->size())
+					return false;
+
+				if (p1.first < p2.first)
+					return true;
+
+				if (p1.first > p2.first)
+					return false;
+
+				return p1.second.get() < p2.second.get();
+
+			}
+
+		};
+
+		typedef typename Util::OrderedAntichain2C<Antichain2C, less> OrderedAntichain2C;
 
 		GCC_DIAG_OFF(effc++)
 		struct Choice {
@@ -518,7 +592,7 @@ public:
 				const Antichain2C::TList& fixed)
 				: processed_(processed), fixed_(fixed), state_() {}
 		
-			bool get(const Explicit::StateTuple& children, size_t index) {
+			bool build(const Explicit::StateTuple& children, size_t index) {
 
 				assert(index < children.size());
 
@@ -624,7 +698,8 @@ public:
 		);
 
 		Antichain1C post;
-		Antichain2C next, processed;
+		Antichain2C processed;
+		OrderedAntichain2C next;
 
 		bool isAccepting;
 
@@ -682,6 +757,7 @@ public:
 				assert(transition->state() < inv.size());
 					
 				next.refine(inv[transition->state()], ptr, gte);
+
 				next.insert(transition->state(), ptr);
 
 			}
@@ -696,7 +772,7 @@ public:
 
 		ChoiceVector choiceVector(processed, fixedList);
 
-		while (next.next(q, Q)) {
+		while (next.get(q, Q)) {
 
 			assert(q < inv.size());
 
@@ -724,7 +800,7 @@ public:
 
 						assert(smallerTransition);
 
-						if (!choiceVector.get(smallerTransition->children(), i))
+						if (!choiceVector.build(smallerTransition->children(), i))
 							continue;
 
 						do {
@@ -779,6 +855,7 @@ public:
 							assert(smallerTransition->state() < inv.size());
 	
 							next.refine(inv[smallerTransition->state()], ptr, gte);
+
 							next.insert(smallerTransition->state(), ptr);
 
 						} while (choiceVector.next());
---
+++ include/vata/util/ordered_antichain2c.hh
@@ -0,0 +1,171 @@
+/*****************************************************************************
+ *  Vojnar's Army Tree Automata Library
+ *
+ *  Copyright (c) 2011 Jiri Simacek <isimacek@fit.vutbr.cz>
+ *
+ *  Description:
+ *    Header file for a class representing antichain.
+ *
+ *****************************************************************************/
+
+#ifndef _VATA_ORDERED_ANTICHAIN_2C_HH_
+#define _VATA_ORDERED_ANTICHAIN_2C_HH_
+
+#include <ostream>
+#include <set>
+
+#include <vata/util/convert.hh>
+
+namespace VATA
+{
+	namespace Util
+	{
+		template <typename Antichain2C, class Less> class OrderedAntichain2C;
+	}
+}
+
+template <class Antichain2C, class Less>
+class VATA::Util::OrderedAntichain2C {
+
+public:
+
+	typedef typename Antichain2C::first_type first_type;
+	typedef typename Antichain2C::second_type second_type;
+	typedef std::pair<first_type, typename Antichain2C::TList::iterator> KeyIteratorPair;
+
+	struct less {
+
+		bool operator()(const KeyIteratorPair& p1, const KeyIteratorPair& p2) const {
+
+			return Less()(
+				std::make_pair(p1.first, *p1.second), std::make_pair(p2.first, *p2.second)
+			);
+
+		}
+
+	};
+	
+	typedef std::set<KeyIteratorPair, less> OrderedType;
+
+private:
+
+	Antichain2C antichain_;
+	OrderedType data_;
+
+public:
+
+	OrderedAntichain2C() : antichain_(), data_() {}
+
+	const typename Antichain2C::TList* lookup(const first_type& key) const {
+
+		return this->antichain_.lookup(key);
+
+	}
+
+	// return TRUE if there exists (p, P) in ac such that q <= p and P <= Q
+	template <class Cont, class Cmp>
+	bool contains(const Cont& candidates /* candidates for p */, const second_type& Q,
+		const Cmp& cmp) const {
+
+		return this->antichain_.contains(candidates, Q, cmp);
+
+	}
+
+	// remove all (p, P) from ac such that q <= p and P <= Q
+	template <class Cont, class Cmp>
+	void refine(const Cont& candidates /* candidates for p */, const second_type& Q,
+		const Cmp& cmp) {
+
+		struct Eraser {
+
+			OrderedType& data_;
+
+			Eraser(OrderedType& data) : data_(data) {}
+
+			void operator()(const first_type& q,
+				const typename Antichain2C::TList::iterator& Q) const {
+
+				assert(this->data_.count(std::make_pair(q, Q)));
+
+				this->data_.erase(std::make_pair(q, Q));
+				
+			}
+
+		};
+
+		this->antichain_.refine(candidates, Q, cmp, Eraser(this->data_));
+
+		assert(this->antichain_.size() == this->data_.size());
+
+	}
+
+	// add (q, Q) to ac
+	typename OrderedType::iterator insert(const first_type& q, const second_type& Q) {
+
+		auto iter = this->antichain_.insert(q, Q);
+
+		assert(this->data_.count(std::make_pair(q, iter)) == 0);
+
+		return this->data_.insert(std::make_pair(q, iter)).first;
+
+	}
+
+	bool get(first_type& q, second_type& Q) {
+
+		assert(this->antichain_.size() == this->data_.size());
+
+		if (this->data_.empty())
+			return false;
+
+		auto i = this->data_.begin();
+
+		q = i->first;
+		Q = *i->second;
+
+		this->antichain_.remove(i->first, i->second);
+
+		this->data_.erase(i);
+
+		assert(this->antichain_.size() == this->data_.size());
+
+		return true;
+
+	}
+
+	const Antichain2C& antichain() const { return this->antichain_; }
+
+	const OrderedType& data() const { return this->data_; }
+
+	void clear() {
+
+		this->antichain_.clear();
+		this->data_.clear();
+
+	}
+
+	inline bool empty() {
+
+		assert(this->antichain_.size() == this->data_.size());
+
+		return this->data_.empty();
+
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const OrderedAntichain2C& ac) {
+
+		os << '{';
+
+		for (auto& smallerBiggerPair : ac.data_) {
+
+			os << " (" << Util::Convert::ToString(smallerBiggerPair.first) << ", " <<
+				Util::Convert::ToString(*smallerBiggerPair.second) << ')';
+
+		}
+
+		return os << " }";
+
+	}
+
+};
+
+#endif
