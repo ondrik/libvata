2682a70f4944e84057393105fc2cac3785e122d2
Jiri Simacek <isimacek@fit.vutbr.cz>
2011-10-18 20:00:45 +0200

    explicit_tree_*.hh: move bottom up indexing outside the main automaton class

--- include/vata/explicit_tree_incl_up.hh
+++ include/vata/explicit_tree_incl_up.hh
@@ -60,14 +60,228 @@ class VATA::ExplicitUpwardInclusion {
 
 	}
 
+	class Transition {
+	
+		Explicit::TuplePtr children_;
+		size_t symbol_;
+		Explicit::StateType state_;
+	
+	public:
+	
+		Transition(const Explicit::TuplePtr& children, const size_t& symbol,
+			const Explicit::StateType& state) : children_(children), symbol_(symbol), state_(state) {}
+	
+		const Explicit::StateTuple& children() const { return *this->children_; }
+		const size_t& symbol() const { return this->symbol_; }
+		const Explicit::StateType& state() const { return this->state_; }
+	
+		bool operator==(const Transition& rhs) const {
+	
+			return this->children_.get() == rhs.children_.get() &&
+				this->symbol_ == rhs.symbol_ &&
+				this->state_ == rhs.state_;
+	
+		}
+	
+		friend std::ostream& operator<<(std::ostream& os, const Transition& t) {
+			return os << t.symbol_ << Util::Convert::ToString(*t.children_) << "->" << t.state_;
+		}
+	
+		friend size_t hash_value(const Transition& t) {
+	
+			size_t seed = 0;
+	
+			boost::hash_combine(seed, t.children_.get());
+			boost::hash_combine(seed, t.symbol_);
+			boost::hash_combine(seed, t.state_);
+	
+			return seed;
+	
+		}
+	
+	};
+
+	typedef std::shared_ptr<Transition> TransitionPtr;
+	typedef std::vector<TransitionPtr> TransitionList;
+	typedef std::vector<TransitionList> IndexedTransitionList;
+	typedef std::vector<IndexedTransitionList> DoubleIndexedTransitionList;
+	typedef std::vector<TransitionList> SymbolToTransitionListMap;
+	typedef std::vector<IndexedTransitionList> SymbolToIndexedTransitionListMap;
+	typedef std::vector<DoubleIndexedTransitionList> SymbolToDoubleIndexedTransitionListMap;
+	typedef std::vector<SymbolToIndexedTransitionListMap> IndexedSymbolToIndexedTransitionListMap;
+
+	template <class Aut, class SymbolIndex>
+	static void bottomUpIndex(const Aut& aut,
+		IndexedSymbolToIndexedTransitionListMap& bottomUpIndex,
+		SymbolToTransitionListMap& leaves, SymbolIndex& symbolIndex) {
+	
+		for (auto& stateClusterPair : *aut.transitions_) {
+	
+			assert(stateClusterPair.second);
+	
+			for (auto& symbolTupleSetPair : *stateClusterPair.second) {
+	
+				assert(symbolTupleSetPair.second);
+				assert(symbolTupleSetPair.second->size());
+	
+				auto& symbol = symbolIndex[symbolTupleSetPair.first];
+	
+				auto& first = *symbolTupleSetPair.second->begin();
+	
+				assert(first);
+	
+				if (first->empty()) {
+	
+					if (leaves.size() <= symbol)
+						leaves.resize(symbol + 1);
+	
+					auto& transitionList = leaves[symbol];
+	
+					for (auto& tuple : *symbolTupleSetPair.second) {
+	
+						assert(tuple);
+						assert(tuple->empty());
+	
+						transitionList.push_back(
+							TransitionPtr(new Transition(tuple, symbol, stateClusterPair.first))
+						);
+	
+					}
+	
+					continue;
+	
+				}
+	
+				for (auto& tuple : *symbolTupleSetPair.second) {
+	
+					assert(tuple);
+					assert(tuple->size());
+	
+					TransitionPtr transition(
+						new Transition(tuple, symbol, stateClusterPair.first)
+					);
+	
+					size_t i = 0;
+	
+					for (auto& state : *tuple) {
+	
+						if (bottomUpIndex.size() <= state)
+							bottomUpIndex.resize(state + 1);
+	
+						auto& symbolIndexedTransitionList = bottomUpIndex[state];
+	
+						if (symbolIndexedTransitionList.size() <= symbol)
+							symbolIndexedTransitionList.resize(symbol + 1);
+	
+						auto& indexedTransitionList = symbolIndexedTransitionList[symbol];
+	
+						if (indexedTransitionList.size() <= i)
+							indexedTransitionList.resize(i + 1);
+	
+						indexedTransitionList[i].push_back(transition);
+	
+						++i;
+	
+					}
+	
+				}
+	
+			}
+	
+		}
+	
+	}
+	
+	template <class Aut, class SymbolIndex>
+	static void bottomUpIndex2(const Aut& aut,
+		SymbolToDoubleIndexedTransitionListMap& bottomUpIndex,
+		SymbolToTransitionListMap& leaves, SymbolIndex& symbolIndex) {
+	
+		for (auto& stateClusterPair : *aut.transitions_) {
+	
+			assert(stateClusterPair.second);
+	
+			for (auto& symbolTupleSetPair : *stateClusterPair.second) {
+	
+				assert(symbolTupleSetPair.second);
+				assert(symbolTupleSetPair.second->size());
+	
+				auto& symbol = symbolIndex[symbolTupleSetPair.first];
+	
+				auto& first = *symbolTupleSetPair.second->begin();
+	
+				assert(first);
+	
+				if (first->empty()) {
+	
+					if (leaves.size() <= symbol)
+						leaves.resize(symbol + 1);
+	
+					auto& transitionList = leaves[symbol];
+	
+					for (auto& tuple : *symbolTupleSetPair.second) {
+	
+						assert(tuple);
+						assert(tuple->empty());
+	
+						transitionList.push_back(
+							TransitionPtr(new Transition(tuple, symbol, stateClusterPair.first))									
+						);
+	
+					}
+	
+					continue;
+	
+				}
+	
+				if (bottomUpIndex.size() <= symbol)
+					bottomUpIndex.resize(symbol + 1);
+	
+				auto& doubleIndexedTransitionList = bottomUpIndex[symbol];
+	
+				if (doubleIndexedTransitionList.size() < first->size())
+					doubleIndexedTransitionList.resize(first->size());
+	
+				for (auto& tuple : *symbolTupleSetPair.second) {
+	
+					assert(tuple);
+					assert(tuple->size());
+	
+					TransitionPtr transition(
+						new Transition(tuple, symbol, stateClusterPair.first)
+					);
+	
+					size_t i = 0;
+	
+					for (auto& state : *tuple) {
+	
+						assert(i < doubleIndexedTransitionList.size());
+	
+						if (doubleIndexedTransitionList[i].size() <= state)
+							doubleIndexedTransitionList[i].resize(state + 1);
+	
+						doubleIndexedTransitionList[i][state].push_back(transition);
+	
+						++i;
+	
+					}
+	
+				}
+	
+			}
+	
+		}
+	
+	}
+
 public:
 
 	template <class Aut, class Rel>
 	static bool Check(const Aut& smaller, const Aut& bigger, const Rel& preorder) {
 
-		typename Aut::IndexedSymbolToIndexedTransitionListMap smallerIndex;
-		typename Aut::SymbolToDoubleIndexedTransitionListMap biggerIndex;
-		typename Aut::SymbolToTransitionListMap smallerLeaves, biggerLeaves;
+		IndexedSymbolToIndexedTransitionListMap smallerIndex;
+		SymbolToDoubleIndexedTransitionListMap biggerIndex;
+		SymbolToTransitionListMap smallerLeaves, biggerLeaves;
 
 		size_t symbolCnt = 0;
 		std::unordered_map<typename Aut::SymbolType, size_t> symbolMap;
@@ -77,15 +291,18 @@ public:
 				[&symbolCnt](const typename Aut::SymbolType&){ return symbolCnt++; }
 			);
 
-		smaller.bottomUpIndex(smallerIndex, smallerLeaves, symbolTranslator);
-		bigger.bottomUpIndex2(biggerIndex, biggerLeaves, symbolTranslator);
+		ExplicitUpwardInclusion::bottomUpIndex(
+			smaller, smallerIndex, smallerLeaves, symbolTranslator
+		);
+
+		ExplicitUpwardInclusion::bottomUpIndex2(
+			bigger, biggerIndex, biggerLeaves, symbolTranslator
+		);
 
 		typedef Explicit::StateType SmallerType;
 		typedef std::vector<Explicit::StateType> StateSet;
 
 		typedef size_t SymbolType;
-		typedef typename Aut::SimplifiedTransition Transition;
-		typedef typename Aut::SimplifiedTransitionPtr TransitionPtr;
 
 		typedef std::unordered_set<const Transition*> TransitionSet;
 		typedef typename std::shared_ptr<TransitionSet> TransitionSetPtr;
