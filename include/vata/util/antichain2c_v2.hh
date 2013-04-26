7d0c4d50b73dd1564728a4137053f5476fd823b2
Ondra Lengal <ondra.lengal@gmail.com>
2013-04-26 17:17:11 +0200

    antichain2c_v2: Add documentation

--- include/vata/util/antichain2c_v2.hh
+++ include/vata/util/antichain2c_v2.hh
@@ -25,9 +25,32 @@ namespace VATA
 	}
 }
 
+/**
+ * @brief  An implementation of a 2-component antichain
+ *
+ * This class implements a 2-component @e antichain, i.e., an antichain over the
+ * domain Key x T. An antichain is a set of elements from the domain which are
+ * pairwise incomparable w.r.t. a given partial order.
+ *
+ * The antichain represents an @e up @e closure of its elements, i.e., the set
+ * of all elements of the domain that are greater than or equal to some element in
+ * the antichain. The notion of @e greater in our case deserves more discussion.
+ * We consider a pair (p, P) to be greater than (q, Q), denoted as
+ * (q, Q) <= (p, P), if and only if: q => p && Q <= P.
+ *
+ * This implementation is targeted for the use in checking language inclusion of
+ * a pair of automata A <= B, where Key is a state of A and T is a set of states
+ * of B. The partial order is a composition of the partial orders on the two
+ * given domains, i.e., for the intended use, the order on A is the identity (or
+ * a simulation) and the order is the set inclusion relation (or set inclusion
+ * enhanced by simulation).
+ *
+ * @tparam  Key  The first domain
+ * @tparam  T    The second domain
+ */
 template <typename Key, typename T>
-class VATA::Util::Antichain2Cv2 {
-
+class VATA::Util::Antichain2Cv2
+{
 public:
 
 	typedef Key first_type;
@@ -37,7 +60,11 @@ public:
 
 protected:
 
-	struct DummyEraser {
+	/**
+	 * @brief  An eraser that does nothing
+	 */
+	struct DummyEraser
+	{
 		void operator()(const Key&, const typename TList::iterator&) const {}
 	};
 
@@ -47,173 +74,347 @@ private:
 
 protected:
 
+	/**
+	 * @brief  Checks whether an iterator is in a given container
+	 *
+	 * This method checks whether an instance of an iterator is inside a given
+	 * container.
+	 *
+	 * @param[in]  c      The container to be checked for the presence of @p iter
+	 * @param[in]  iter   The iterator the presence of which is to be checked
+	 *
+	 * @returns  @p true in the case @p iter points inside @p c, @p false
+	 *           otherwise
+	 */
 	template <class Cont>
-	static bool checkIteratorPresence(Cont& c, const typename Cont::iterator& iter) {
-
-		for (auto i = c.begin(); i != c.end(); ++i) {
-
-			if (i == iter)
+	static bool checkIteratorPresence(const Cont& c, const typename Cont::iterator& iter)
+	{
+		for (auto it = c.begin(); it != c.end(); ++it)
+		{
+			if (it == iter)
 				return true;
-
 		}
 
 		return false;
-
 	}
 
 public:
 
+	/**
+	 * @brief  Default constructor
+	 *
+	 * Default constructor.
+	 */
 	Antichain2Cv2() : data_() {}
 
-	void swap(Antichain2Cv2& rhs) {
 
-		std::swap(this->data_, rhs.data_);
-
+	/**
+	 * @brief  Swaps the content with another antichain
+	 *
+	 * Swaps the content of the antichain with another antichain.
+	 *
+	 * @param[in, out]  rhs  The other antichain
+	 */
+	void swap(
+		Antichain2Cv2&                rhs)
+	{
+		std::swap(data_, rhs.data_);
 	}
 
-	const TList* lookup(const Key& key) const {
 
-		auto i = this->data_.find(key);
+	/**
+	 * @brief  Looks up a list of elements associated with a key
+	 *
+	 * Looks up a list of elements of T that are stored in the antichain in pairs
+	 * where the first component of the pair is @p key.
+	 *
+	 * @param[in]  key  The first component of the pairs in the antichain
+	 *
+	 * @return  A pointer to a list of elements of T that are as the second
+	 *          component in a pair with @p key, @p nullptr if there are none
+	 */
+	const TList* lookup(const Key& key) const
+	{
+		auto it = data_.find(key);
 
-		return (i == this->data_.end())?(nullptr):(&i->second);
-
+		return (data_.end() == it) ? (nullptr) : (&it->second);
 	}
 
-	// return TRUE if there exists (p, P) in ac such that q <= p and P <= Q
+
+	/**
+	 * @brief  Checks whether a pair (q, Q) is covered by the antichain
+	 *
+	 * This method determines whether a pair (q, Q) is covered by the antichain.
+	 * In particular, it is given a collection of @p candidates for 'p' (such
+	 * that p => q) and tries to find in the antichain a pair '(p, P)' such
+	 * that P <= @p Q. The '<=' relation on elements of T is given by @p cmp.
+	 * If there is such, the method returns @p true, otherwise it returns @p
+	 * false.
+	 *
+	 * @param[in]  candidates  Candidates for 'p'
+	 * @param[in]  Q           The 'Q' in the pair '(q, Q)'
+	 * @param[in]  cmp         The '<=' comparison operation on elements of T
+	 *
+	 * @returns  @p true if '(q, Q)' is covered by the antichain, @p false
+	 *           otherwise
+	 *
+	 * @note  The relation @p cmp needs to be complementary to the one used in
+	 *        the refine() method!
+	 */
 	template <class Cont, class Cmp>
-	bool contains(const Cont& candidates /* candidates for p */, const T& Q, const Cmp& cmp) const {
-
-		for (auto& p : candidates) {
-
-			auto i = this->data_.find(p);
-
-			if (i == this->data_.end())
+	bool contains(
+		const Cont&                  candidates,
+		const T&                     Q,
+		const Cmp&                   cmp) const
+	{
+		for (const Key& p : candidates)
+		{	// check all candidates for 'p'
+			auto iter = data_.find(p);
+			if (data_.end() == iter)
+			{	// in the case there is no pair (p, _) in the antichain
 				continue;
-
-			for (auto& P : i->second) {
-
-				if (cmp(P, Q))
-					return true;
-
 			}
 
+			for (auto& P : iter->second)
+			{	// for all 'P' such that (p, P) is in the antichain, check whether P <= Q
+				if (cmp(P, Q))
+				{	// if P <= Q
+					return true;
+				}
+			}
 		}
 
 		return false;
-
 	}
 
-	// remove all (p, P) from ac such that q <= p and P <= Q
+
+	/**
+	 * @brief  Removes all pairs subsumed by (q, Q) from the antichain
+	 *
+	 * This method removes all pairs (p, P) covered by (q, Q) from the antichain.
+	 * In particular, it is given a collection of @p candidates for 'p' (such that
+	 * p <= q) and removes from the antichain all pairs '(p, P)' such that P => @p
+	 * Q. The '=>' relation on elements of T is given by @p cmp. An optional
+	 * @p eraser on the removed elements is executed.
+	 *
+	 * @param[in]  candidates  Candidates for 'p'
+	 * @param[in]  Q           The 'Q' in the pair '(q, Q)'
+	 * @param[in]  cmp         The '=>' comparison operation on elements of T
+	 * @param[in]  eraser      An optional eraser for removed elements
+	 *
+	 * @note  The relation @p cmp needs to be complementary to the one used in
+	 *        the contains() method!
+	 */
 	template <class Cont, class Cmp, class Eraser = DummyEraser>
-	void refine(const Cont& candidates /* candidates for p */, const T& Q, const Cmp& cmp,
-		const Eraser& eraser = DummyEraser()) {
-
-		for (auto& p : candidates) {
-
-			auto i = this->data_.find(p);
-
-			if (i == this->data_.end())
+	void refine(
+		const Cont&          candidates,
+		const T&             Q,
+		const Cmp&           cmp,
+		const Eraser&        eraser = DummyEraser())
+	{
+		for (const Key& p : candidates)
+		{	// check all candidates for 'p'
+			auto iter = data_.find(p);
+			if (data_.end() == iter)
+			{	// in the case there is no pair (p, _) in the antichain
 				continue;
-
-			for (auto j = i->second.begin(); j != i->second.end(); ) {
-
-				auto k = j++;
-
-				if (cmp(*k, Q)) {
-
-					eraser(p, k);
-
-					i->second.erase(k);
-
-				}
-
 			}
 
-			if (i->second.empty())
-				this->data_.erase(i);
+			for (auto jter = iter->second.begin(); jter != iter->second.end(); )
+			{	// for all 'P' such that (p, P) is in the antichain, check whether P <= Q
+				auto kter = jter++;
 
+				if (cmp(*kter, Q))
+				{	// if P <= Q, erase (p, P) from the antichain
+					eraser(p, kter);
+
+					// we rely on the fact that erase() does not invalidate jter
+					iter->second.erase(kter);
+				}
+			}
+
+			if (iter->second.empty())
+			{	// in case there is no (p, _) left, remove 'p'
+				data_.erase(iter);
+			}
 		}
-
 	}
 
-	// add (q, Q) to ac
-	typename TList::iterator insert(const Key& q, const T& Q) {
 
-		auto& list = this->data_.insert(std::make_pair(q, TList())).first->second;
+	/**
+	 * @brief  Adds a pair (q, Q) in the antichain
+	 *
+	 * This method inserts a new pair '(@p q, @p Q)' into the antichain and
+	 * returns an iterator at the inserted element.
+	 *
+	 * @param[in]  q  The 'q' in '(q, Q)'
+	 * @param[in]  Q  The 'Q' in '(q, Q)'
+	 *
+	 * @returns  An iterator at the inserted element
+	 */
+	typename TList::iterator insert(
+		const Key&          q,
+		const T&            Q)
+	{
+		TList& list = data_.insert(std::make_pair(q, TList())).first->second;
 
 		return list.insert(list.end(), Q);
-
 	}
 
-	bool get(Key& q, T& Q) {
 
-		if (this->data_.empty())
+	/**
+	 * @brief  Retrieves an arbitrary element from the antichain
+	 *
+	 * Retrieves an arbitrary pair '(@p q, @p Q)' from the antichain. Returns @p
+	 * true if there is some, @p false otherwise.
+	 *
+	 * @param[out]  q  The 'q' in '(q, Q)'
+	 * @param[out]  Q  The 'Q' in '(q, Q)'
+	 *
+	 * @returns  @p true if there is some element in the antichain, @p false
+	 *           otherwise
+	 */
+	bool get(
+		Key&         q,
+		T&           Q)
+	{
+		if (data_.empty())
+		{	// stop immediately in the case the antichain is empty
+			assert(data_.cbegin() != data_.cend());
 			return false;
+		}
 
-		auto i = this->data_.begin();
+		auto iter = data_.begin();
+		// make sure there is something
+		assert(data_.end() != iter);
 
-		q = i->first;
-		Q = i->second.front();
+		q = iter->first;
 
-		i->second.pop_front();
+		// make sure there is something
+		assert(!iter->second.empty());
+		Q = iter->second.front();
 
-		if (i->second.empty())
-			this->data_.erase(i);
+		iter->second.pop_front();
+
+		if (iter->second.empty())
+		{
+			data_.erase(iter);
+		}
 
 		return true;
-
 	}
 
-	void remove(const Key& q, const typename TList::iterator& Q) {
 
-		auto iter = this->data_.find(q);
+	/**
+	 * @brief  Removes a pair (q, iterator) from the antichain
+	 *
+	 * This method removes the pair '(q, Q)', where @p Q is given by an iterator
+	 * pointing at its position, from the antichain
+	 *
+	 * @param[in]  q  The 'q' in '(q, Q)'
+	 * @param[in]  Q  The iterator pointing at 'Q' in '(q, Q)'
+	 */
+	void remove(const Key& q, const typename TList::iterator& Q)
+	{
+		auto iter = data_.find(q);
 
-		assert(iter != this->data_.end());
+		assert(data_.end() != iter);
 		assert(Antichain2Cv2::checkIteratorPresence(iter->second, Q));
 
 		iter->second.erase(Q);
 
 		if (iter->second.empty())
-			this->data_.erase(iter);
-
+		{
+			data_.erase(iter);
+		}
 	}
 
-	const KeyToTListMap& data() const { return this->data_; }
 
-	size_t size() const {
+	/**
+	 * @brief  Retrieves the data of the antichain
+	 *
+	 * Returns the data of the antichain, i.e., the contained structure.
+	 *
+	 * @returns  The data of the antichain.
+	 */
+	const KeyToTListMap& data() const
+	{
+		return data_;
+	}
 
+
+	/**
+	 * @brief  The size of the antichain
+	 *
+	 * Returns the total number of elements in the antichain.
+	 *
+	 * @returns  The total number of elements in the antichain
+	 */
+	size_t size() const
+	{
 		size_t size = 0;
 
-		for (auto& p : this->data_)
-			size += p.second.size();
+		for (const auto& keyElemPair : data_)
+		{
+			size += keyElemPair.second.size();
+		}
 
 		return size;
-
 	}
 
-	void clear() { this->data_.clear(); }
 
-	inline bool empty() { return this->data_.empty();}
+	/**
+	 * @brief  Clears the antichain
+	 *
+	 * Removes all elements from the antichain.
+	 */
+	void clear()
+	{
+		this->data_.clear();
+	}
 
-	friend std::ostream& operator<<(std::ostream& os, const Antichain2Cv2& ac) {
 
+	/**
+	 * @brief  Checks whether there is no element
+	 *
+	 * This method checks whether there is no element in the antichain.
+	 *
+	 * @returns  @p true in the case the antichain is empty, @p false otherwise
+	 */
+	inline bool empty()
+	{
+		return data_.empty();
+	}
+
+
+	/**
+	 * @brief  Stream output operator
+	 *
+	 * Serializes the content of the antichain into a stream.
+	 *
+	 * @param[in, out]  os  The output stream
+	 * @param[in]       ac  The antichain to be serialized
+	 *
+	 * @returns  Modified output stream
+	 */
+	friend std::ostream& operator<<(std::ostream& os, const Antichain2Cv2& ac)
+	{
 		os << '{';
 
-		for (auto& smallerBiggerListPair : ac.data_) {
-
+		for (auto& smallerBiggerListPair : ac.data_)
+		{
 			os << " (" << smallerBiggerListPair.first << ", {";
 
-			for (auto& element : smallerBiggerListPair.second)
+			for (const T& element : smallerBiggerListPair.second)
+			{
 				os << ' ' << Util::Convert::ToString(element);
+			}
 
 			os << " })";
-
 		}
 
 		return os << " }";
-
 	}
-
 };
 
 #endif
