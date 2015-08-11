effe58e8d5f6f974551226a8ba2983051ffd8d8e
Martin Hruska <hruskamartin25@gmail.com>
2015-08-11 11:01:27 +0200

    testing printing

--- python_interface/tester.py
+++ python_interface/tester.py
@@ -5,6 +5,8 @@ import vata_result
 
 res = [vata_interface.inclusion('../tests/aut_timbuk_smaller/A0053', \
         '../tests/aut_timbuk_smaller/A0054')]
+with open("temp.txt",'w') as f:
+    res[0].writeStderrToFile(f)
 res.append(vata_interface.union('../tests/aut_timbuk_smaller/A0053', \
         '../tests/aut_timbuk_smaller/A0054'))
 res.append(vata_interface.intersection('../tests/aut_timbuk_smaller/A0053', \
